a3c14f3fac2ccc5912dc9e566152cc75f7cdb45b
Nicholas Barrios <nicholas.barrios@post.ca.gov>
2025-08-08 13:59:20 -0700

    Set shell to Powershell on Windows

--- init.lua
+++ init.lua
@@ -26,6 +26,20 @@ end
 require "lazy_setup"
 require "polish"
 
+if vim.fn.has "win32" then
+  if vim.fn.executable "pwsh" == 1 then
+    vim.opt.shell = "pwsh"
+  else
+    vim.opt.shell = "powershell"
+  end
+  vim.opt.shellcmdflag =
+    "-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering='plaintext';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
+  vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+  vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
 if vim.g.neovide then
   vim.o.guifont = "Monaspace Neon Var:h16"
   vim.g.neovide_opacity = 0.6
